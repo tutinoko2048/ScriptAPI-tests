0448bcb027c0c4fa1242966e41e8b3b9ae1ff7fd
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-08-18 09:30:59 +0000

    new version

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo -n Put module version: @minecraft/server@
 read version
 echo installing @minecraft/server@$version ...
 npm i @minecraft/server@$version
-cp ./node_modules/@minecraft/server/index.d.ts ./Types/index.d.ts
-echo saved typing file as ./Types/index.d.ts
+cp ./node_modules/@minecraft/server/index.d.ts ./Types/$version.d.ts
+echo saved typing file as ./Types/$version.d.ts
