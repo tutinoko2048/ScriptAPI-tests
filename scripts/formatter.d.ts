36b1bcb76187232814f8e769e6f910f4192c3eb4
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-09-10 11:44:29 +0000

    class-stringifier

---
+++ scripts/formatter.d.ts
@@ -0,0 +1,8 @@
+interface FormatterOptions {
+    maxDepth?: number;
+    lineLength?: number;
+    arrayIndex?: boolean;
+    hideFunction?: boolean;
+}
+export declare function format(value: any, options: FormatterOptions): string;
+export {};
