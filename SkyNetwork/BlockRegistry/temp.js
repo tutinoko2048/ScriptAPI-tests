0bd3c801dee736024c7e633ca3973bd853107147
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-11-23 12:53:19 +0900

    Add files via upload

--- SkyNetwork/BlockRegistry/temp.js
+++ SkyNetwork/BlockRegistry/temp.js
@@ -1,10 +1,17 @@
+// いい感じに組み込むなりファイル分けするなりしてください
+
 import { LocationInUnloadedChunkError, system, world, Direction, Vector } from '@minecraft/server';
-import { BlockPlaceRegistry } from './BlockPlaceRegistry';
-const Permissions = {
-  has(...args) {return true;}
-}
+import { BlockPlaceRegistry } from './BlockPlaceRegistry'; // BlockRegistryフォルダからインポート
+
+
+
+// エラー回避用の仮変数だから消してね
+const Permissions = { has(...args) {return true;} }
 const dimension = world.getDimension('overworld');
 const setScore = (...args) => {}
+
+
+
 
 world.afterEvents.playerPlaceBlock.subscribe(ev => {
   const { player, block } = ev;
@@ -17,9 +24,6 @@ world.afterEvents.playerPlaceBlock.subscribe(ev => {
     }
   }
 });
-
-// native classはJSON.stringifyできないからオブジェクトに変換
-const vec3 = ({ x, y, z }) => ({ x, y, z });
 
 /** @type {Record<Direction, Vector>} */
 const directionToRelative = {
@@ -39,15 +43,31 @@ const liquids = [
 world.afterEvents.itemUseOn.subscribe(ev => {
   if (!liquids.includes(ev.itemStack.typeId)) return;
   // 触ったブロックと触った面を元に置いた液体の座標を計算
-  const placedLocation = Vector.add(ev.block.location, directionToRelative[ev.blockFace]);
-  
-  BlockPlaceRegistry.put(vec3(placedLocation));
+  const { x, y, z } = Vector.add(ev.block.location, directionToRelative[ev.blockFace]);
+  try {
+      BlockPlaceRegistry.put({ x, y, z }); // native classはJSON.stringifyできないからオブジェクトに変換
+  } catch (e) {
+    ev.source.sendMessage(`§cエラーが発生しました。以下のコードを管理者にお知らせください。\n${e}`);
+    console.error(e, e.stack);
+  }
 });
+
+
+
 
 system.afterEvents.scriptEventReceive.subscribe(ev => {
   const { id } = ev;
   if (id === "bw:map_RESET") {
+    // ...
+
+    // ブロック巻き戻し関連は関数にまとめた
     resetMap();
+  }
+
+  // 確認用にこれも追加しておいてほしい
+  if (id === "debug:log_place_registry_count") {
+    const locations = BlockPlaceRegistry.get();
+    console.warn(`[BlockPlaceRegistry] current blocks: ${locations.length}`);
   }
 });
 
@@ -65,7 +85,7 @@ function resetMap() {
     }
     const location = queue.shift();
 
-    /** @type {import('@minecraft/server').Block} */
+    /** @type {import('@minecraft/server').Block|undefined} */
     let block;
     try {
       block = dimension.getBlock(location);
