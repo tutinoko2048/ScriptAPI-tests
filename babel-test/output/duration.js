1c5a567ada328df5cf2aec65c72e016fb45d65a6
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-12-15 11:17:23 +0000

    test: regex変換できないか試したけどだめだった

---
+++ babel-test/output/duration.js
@@ -0,0 +1,38 @@
+const RegExpTree = require('regexp-tree')
+RegExpTree.exec('', {
+  
+})
+const text = '';
+const durations = {
+  y: {
+    time: 365 * 24 * 60 * 60 * 1000,
+    long: 'year'
+  },
+  w: {
+    time: 7 * 24 * 60 * 60 * 1000,
+    long: 'week'
+  },
+  d: {
+    time: 24 * 60 * 60 * 1000,
+    long: 'day'
+  },
+  h: {
+    time: 60 * 60 * 1000,
+    long: 'hour'
+  },
+  m: {
+    time: 60 * 1000,
+    long: 'minute'
+  },
+  s: {
+    time: 1000,
+    long: 'second'
+  },
+  ms: {
+    time: 1,
+    long: 'millisecond'
+  }
+};
+text.replace(/\s+/g, '').match(RegExp(Object.entries(durations).reduce((p, [short, {
+  long
+}]) => p + `((?<${short}>-?(\\d*\\.\\d+|\\d+))(${short}|${long}))?`, '') + '$', 'i'));
