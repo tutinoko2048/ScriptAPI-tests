3b24cd1bef88d8ceb7e22b6eef85c4331e00670b
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-01-18 09:30:32 +0000

    .

--- ScoreboardExtractor/main.js
+++
@@ -1,8 +0,0 @@
-const { Scoreboard } = require("./src/Scoreboard");
-const data = require("./scoreboard.json");
-
-const toJson = require("./src/toJson");
-
-const scoreboard = new Scoreboard(data);
-const objective = scoreboard.getObjective('jaylydb:gotRequests');
-console.log(toJson(objective.getScores()))
